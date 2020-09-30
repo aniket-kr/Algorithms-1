0fd72b9c88d1bcbb90d3c3a47486a90b68e5bd2d
Aniket Kumar <aniket24kr@gmail.com>
2020-09-30 13:31:52 +0530

    Write extended API for OrderMap.java interface
    Wrote the method signatures for OrderMap.java interface and made
    sub-interface tweaks to some methods like `copy` and `deepcopy`.
    The javadocs for OrderMap.java are NOT complete yet. The snippets
    have merely been copied and pasties from the super-interface
    Map.java.

---
+++ src/com/company/aniketkr/algorithms1/map/OrderMap.java
@@ -0,0 +1,194 @@
+package com.company.aniketkr.algorithms1.map;
+
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+public interface OrderMap<K, V> extends Map<K, V> {
+
+  /* **************************************************************************
+   * Section: Object Operations
+   ************************************************************************** */
+
+  @Override
+  int hashCode();
+
+  @Override
+  boolean equals(Object obj);
+
+  @Override
+  String toString();
+
+  /* **************************************************************************
+   * Section: Basic Operations
+   ************************************************************************** */
+
+  /**
+   * Get the count of number of key-value pairs in the map.
+   *
+   * @return Number of key-value pairs in the map.
+   */
+  @Override
+  int size();
+
+  /**
+   * Check if the map is empty.
+   *
+   * @return {@code true} if the map has exactly {@code 0} key-value pairs,
+   *     {@code false} otherwise.
+   */
+  @Override
+  boolean isEmpty();
+
+  /**
+   * Clears the map of all its key-value pairs.
+   * Optionally, it may set the internal state of the map to the default
+   * initialisation state.
+   */
+  @Override
+  void clear();
+
+  /**
+   * Check if {@code key} has some value mapped to it in the map.
+   *
+   * @param key The key.
+   * @return {@code true} if {@code key} has mapping in the map, {@code false}
+   *     otherwise.
+   */
+  @Override
+  boolean contains(K key);
+
+  /* **************************************************************************
+   * Section: Map Operations
+   ************************************************************************** */
+
+  /**
+   * Get the value associated with {@code key} from the map, provided
+   * {@code key} exists in the map.
+   *
+   * @param key The best form of user interface,
+   * @return The associated value.
+   * @throws NoSuchElementException If {@code key} does not exist
+   *                                in the map.
+   */
+  @Override
+  V get(K key);
+
+  /**
+   * Get the value associated with {@code key} from the map, if {@code key}
+   * exists in the map. If not, then returns {@code fallback} value.
+   *
+   * @param key      The key.
+   * @param fallback The "value" to return when {@code key} doesn't exist.
+   * @return The value associated with {@code key} or {@code fallback} depending
+   *     on existence of {@code key} in the map.
+   */
+  @Override
+  V get(K key, V fallback);
+
+  /**
+   * Puts the given key-value pair in the map.
+   * If {@code key} happens to already exist in the map, then the associated
+   * value is updated to {@code value}.
+   *
+   * @param key   The key to put or update in the map.
+   * @param value The value to associate with {@code key}.
+   * @return {@code true} if {@code key} did not exist in the map and a new item
+   *     was inserted, {@code false} if only the associated value was updated.
+   *     In other words, if {@link #size()} increased then {@code true} is
+   *     returned.
+   */
+  @Override
+  boolean put(K key, V value);
+
+  /**
+   * Delete {@code key} and its associated value from the map.
+   * If {@code key} does not exist in the map, then no exception is thrown.
+   *
+   * @param key The key (and associated value) to delete.
+   * @return {@code true} if a deletion took place, {@code false} otherwise.
+   *     In other words, if {@link #size()} decreased then {@code true} is
+   *     returned.
+   */
+  @Override
+  boolean delete(K key);
+
+  /* **************************************************************************
+   * Section: OrderMap Operations
+   ************************************************************************** */
+
+  K min();
+
+  K max();
+
+  K floor(K key);
+
+  K ceil(K key);
+
+  int rank(K key);
+
+  K select(int rank);
+
+  void deleteMin();
+
+  void deleteMax();
+
+  /* **************************************************************************
+   * Section: Duplication Operations
+   ************************************************************************** */
+
+
+  /** Returns a shallow copy of this map. */
+  @Override
+  OrderMap<K, V> copy();
+
+  /**
+   * Returns a deepcopy of the map.
+   *
+   * @param keyCopyFn   A pure function that takes the original key as an
+   *                    argument and returns a deepcopy of the key.
+   * @param valueCopyFn A pure function that takes the original value as an
+   *                    argument and returns a deepcopy of the value.
+   * @return A deepcopy of this map.
+   * @throws IllegalArgumentException If either {@code keyCopyFn} or
+   *                                  {@code valueCopyFn} is {@code null}.
+   */
+  @Override
+  OrderMap<K, V> deepcopy(Function<? super K, K> keyCopyFn, Function<? super V, V> valueCopyFn);
+
+  /* **************************************************************************
+   * Section: Iteration Operations
+   ************************************************************************** */
+
+  /**
+   * Iterate over all the keys of the map in no specific order.
+   * Two consecutive iterations may or may not yield the keys in the same order.
+   *
+   * @return An iterable that iterates over keys.
+   */
+  @Override
+  Iterable<K> keys();
+
+  Iterable<K> keys(K low, K high);
+
+  /**
+   * Iterate over all the associated values of the map in no specific order.
+   * Two consecutive iterations may or may not yield the values in the same
+   * order.
+   *
+   * @return An iterable that iterates over values.
+   */
+  @Override
+  Iterable<V> values();
+
+  /**
+   * Iterate over all the "entries" of the map in no specific order.
+   * An <em>entry</em> is defined as a single key-value pair in the map.
+   *
+   * @return An iterable that iterates over entries, i.e. key-value pairs.
+   * @see Entry
+   */
+  @Override
+  Iterable<Entry<? super K, ? super V>> entries();
+
+  Iterable<Entry<? super K, ? super V>> entries(K low, K high);
+}
