c438440035c2e0ed06bbf7c013965c9db9c81621
Aniket Kumar <aniket24kr@gmail.com>
2020-10-09 21:27:00 +0530

    Complete Collection.java API docs
    The following changes have been made in this commit:
     1. Refactored common functionality of equals() method into a
        protected method equalsHelper().
     2. Added javadocs for all methods and class itself.

--- src/com/company/aniketkr/algorithms1/collection/Collection.java
+++ src/com/company/aniketkr/algorithms1/collection/Collection.java
@@ -6,6 +6,13 @@ import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+
+/**
+ * Abstract base class for all data structures that are containers for similar type of elements.
+ *
+ * @param <E> Type of element in the collection. Unless ordering is explicitly required,
+ *            {@code null} elements are allowed.
+ */
 public abstract class Collection<E> implements Iterable<E> {
 
   /* **************************************************************************
@@ -27,28 +34,21 @@ public abstract class Collection<E> implements Iterable<E> {
     return (int) (hash % Integer.MAX_VALUE);
   }
 
+  /**
+   * Compare the given Object {@code obj} with "this" collection. {@code obj} will only be
+   * considered equal to this collection is all of the following conditions are satisfied:
+   * <ul>
+   *   <li>{@code obj} extends the {@link Collection} abstract class.</li>
+   *   <li>{@code obj} is the same "type" of collection as "this". (Stack goes with Stack,
+   *       Queue with Queue, etc.)</li>
+   *   <li>All the elements in the iteration sequence are equal in both the collections.</li>
+   * </ul>
+   *
+   * @param obj The object to compare with for equality.
+   * @return {@code true} if all above conditions hold, {@code false} otherwise.
+   */
   @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (!(obj instanceof Collection)) {
-      return false;
-    }
-    Collection<?> collection = (Collection<?>) obj;
-    if (this.size() != collection.size()) {
-      return false;
-    }
-
-    // compare all elements
-    Iterator<E> itor = this.iterator();
-    for (Object elmt : collection) {
-      if (!Objects.deepEquals(elmt, itor.next())) {
-        return false;
-      }
-    }
-    return true;
-  }
+  public abstract boolean equals(Object obj);  // REFERENCE: equalsHelper()
 
   @Override
   public String toString() {
@@ -64,12 +64,32 @@ public abstract class Collection<E> implements Iterable<E> {
    * Section: Collection Operations
    ************************************************************************** */
 
+  /**
+   * Gets the number of elements in the collection.
+   *
+   * @return The count of elements.
+   */
   public abstract int size();
 
+  /**
+   * Checks if the collection is empty. A collection is empty when there are no elements in it.
+   *
+   * @return {@code true} if collection has no elements, {@code false} otherwise.
+   */
   public abstract boolean isEmpty();
 
+  /**
+   * Clears the collection of all its elements and set the state of the collection to its
+   * <em>default</em> initialisation state.
+   */
   public abstract void clear();
 
+  /**
+   * Checks if collection has {@code elmt} as one of its elements.
+   *
+   * @param elmt The element to check existence of.
+   * @return {@code true} if {@code elmt} appears at least once in the collection.
+   */
   public boolean contains(E elmt) {
     for (E collectionElmt : this) {
       if (Objects.deepEquals(elmt, collectionElmt)) {
@@ -83,14 +103,35 @@ public abstract class Collection<E> implements Iterable<E> {
    * Section: Duplication Operations
    ************************************************************************** */
 
+  /**
+   * Gets a shallow copy of this collection. A shallow copy copies the collection object but uses
+   * references for the actual elements in the collection, implying that if an element is mutated in
+   * one collection, the element in the copy it also mutated.
+   *
+   * @return A copy of the collection.
+   */
   public abstract Collection<E> copy();
 
+  /**
+   * Gets a deepcopy of this collection. A deepcopy copies the collection object as well as its
+   * elements over to a new collection. Any change made to one is completed isolated of the other.
+   *
+   * @param copyFn A pure-function that accepts an element as an argument and returns its deepcopy.
+   * @return A deepcopy of the collection.
+   * @throws IllegalArgumentException If {@code copyFn} is {@code null}.
+   */
   public abstract Collection<E> deepcopy(Function<? super E, E> copyFn);
 
   /* **************************************************************************
    * Section: Iteration Operations
    ************************************************************************** */
 
+  /**
+   * Iterate over the elements of the collection in natural order. The natural order is best defined
+   * by the extending subclasses.
+   *
+   * @return An iterator that iterates over elements.
+   */
   @Override
   public abstract Iterator<E> iterator();
 
@@ -98,8 +139,7 @@ public abstract class Collection<E> implements Iterable<E> {
    * Section: Protected Helper Methods
    ************************************************************************** */
 
-  protected <C extends Collection<E>> void deepcopyMaker(Function<? super E, E> copyFn, //
-                                                         Consumer<E> addElmt) {
+  protected void deepcopyMaker(Function<? super E, E> copyFn, Consumer<E> addElmt) {
     if (copyFn == null) {
       throw new IllegalArgumentException("param 'copyFn' cannot be null");
     }
@@ -107,5 +147,34 @@ public abstract class Collection<E> implements Iterable<E> {
     for (E elmt : this) {
       addElmt.accept(copyFn.apply(elmt));
     }
+  }
+
+  /**
+   * Checks all of the required conditions for {@link #equals(Object)} one-by-one.
+   *
+   * @param obj      The object to compare with.
+   * @param reqClass The class that the obj should be {@code instanceof} to pass.
+   * @return {@code true} if all {@link #equals(Object) conditions} hold, false otherwise.
+   */
+  protected boolean equalsHelper(Object obj, Class<?> reqClass) {
+    if (this == obj) {
+      return true;
+    }
+    if (!reqClass.isInstance(obj)) {
+      return false;
+    }
+    Collection<?> collection = (Collection<?>) obj;
+    if (this.size() != collection.size()) {
+      return false;
+    }
+
+    // compare all elements
+    Iterator<E> itor = this.iterator();
+    for (Object elmt : collection) {
+      if (!Objects.deepEquals(elmt, itor.next())) {
+        return false;
+      }
+    }
+    return true;
   }
 }
